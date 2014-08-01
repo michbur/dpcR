d62b38ee95a1189e2c24ea723ba234282db7e6a4
michbur <michalburdukiewicz@gmail.com>
2014-08-01 10:13:52 +0200

    Added rtadpcr class.

---
+++ R/rtadpcr.R
@@ -0,0 +1,3 @@
+setClass("rtadpcr", contains = "matrix", representation(.Data = "matrix",
+                                                      n = "integer",
+                                                      type = "character"))
