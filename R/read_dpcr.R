ee71fb745305279088b42341381da3a8cd260004
michbur <michalburdukiewicz@gmail.com>
2015-08-13 17:49:17 +0200

    added read_QX100

--- R/read_dpcr.R
+++ R/read_dpcr.R
@@ -13,7 +13,8 @@
 read_dpcr <- function(file, format, ...) {
   switch(format,
          raw_adpcr = read_raw(file, adpcr = TRUE, ...),
-         raw_ddpcr = read_raw(file, adpcr = FALSE, ...))
+         raw_ddpcr = read_raw(file, adpcr = FALSE, ...),
+         QX100 = read_QX100(file))
 }
 
 #' Read digital PCR raw data
@@ -21,7 +22,7 @@ read_dpcr <- function(file, format, ...) {
 #' Reads digital PCR data in raw format.
 #' 
 #' @inheritParams create_dpcr
-#' @param file name of the input file.
+#' @inheritParams read_dpcr
 #' @author Michal Burdukiewcz, Stefan Roediger
 #' @export
 
@@ -34,4 +35,27 @@ read_raw <- function(file, adpcr) {
   
   create_dpcr(data = as.matrix(dat), n = n, exper = exp_rep[, 1], replicate = exp_rep[, 2], type = "np",
               adpcr = adpcr)
+}
+
+
+#' Read QX100
+#' 
+#' Reads digital PCR data from the QX100 machine.
+#' 
+#' @inheritParams create_dpcr
+#' @param file name of the input file.
+#' @author Michal Burdukiewcz, Stefan Roediger
+#' @export
+
+read_QX100 <- function(file) {
+  dat <- read.csv(file)
+  
+  n <- dat[["Positives"]] + dat[["Negatives"]]
+  counts <- matrix(dat[["Positives"]], nrow = 1)
+  exper <- dat[["TypeAssay"]]
+  replicate <- dat[["Sample"]]
+  
+  create_dpcr(data = matrix(dat[["Positives"]], nrow = 1), n = n, 
+              exper = exper, replicate = replicate, type = "tnp",
+              adpcr = TRUE)
 }
