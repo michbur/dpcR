788f129e741a45826287f76e3ef3e78c4484e57b
devSJR <stefan.roediger@hs-lausitz.de>
2015-08-20 13:40:57 +0200

    data

---
+++ R/White.R
@@ -0,0 +1,34 @@
+#' Digitalized Data from a Fluidigm Array
+#' 
+#' These are the results data from the \code{White} data as measured by the
+#' UT digital PCR on Fluidigm 12.765 digital Array. The data were digtilized
+#' from a supplementory figure "1471-2164-10-116-S1.pdf" 
+#' by White et al. (2009) BMC Genomics
+#' 
+#' Setup: Experimental details were described be White et al. (2009) BMC 
+#' Genomics. The digitalization of the figure was done with imageJ and the 
+#' "MicroArray Profile" plugin by Bob Dougherty (rpd@optinav.com) and 
+#' Wayne Rasband.
+#'
+#' Annotation: TBD
+#' 
+#' @name White
+#' @docType data
+#' @format A data frame with some observations on some variables.
+#' \describe{ Here is nothing to describe }
+#' @author Stefan Roediger, Michal Burdukiewcz, White et al. (2009) BMC Genomics
+#' @references White RA, Blainey PC, Fan HC, Quake SR. Digital PCR provides 
+#' sensitive and absolute calibration for high throughput sequencing. 
+#' BMC Genomics 2009;10:116. doi:10.1186/1471-2164-10-116.
+#'
+#' Dougherty B, Rasband W. MicroArray Profile ImageJ Plugin n.d. 
+#' http://www.optinav.com/imagej.html (accessed August 20, 2015).
+#'
+#' @source Data were digitalized from the supplement material 
+#' "1471-2164-10-116-S1.pdf" by White et al. (2009) BMC Genomics
+#' @keywords datasets
+#' @examples
+#' 
+#' #str(White)
+#' 
+NULL
