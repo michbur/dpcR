c2d17fbfaaf9a21b651961e48ad36e86ae64fec7
michbur <michalburdukiewicz@gmail.com>
2015-09-01 13:26:12 +0200

    moments partially improved

--- R/moments.R
+++ R/moments.R
@@ -56,85 +56,51 @@ moments <- function (input) {
   stop("Wrong class of 'input'")
 }
 
-setMethod("moments", signature(input = "numeric"), function(input) {
-  if (length(input) == 1) {
-    #input contains only number of positive partitions and total number of partitions
-    moms(input)
-  }
-  if (length(input) == 2) {
-    #input contains only number of positive partitions and total number of partitions
-    moms(fl(input[1]/input[2]))
-  } else {
-    res <- cbind(moms(fl(sum(input > 0)/length(input))), empir_moms(input))
-    colnames(res) <- c("Theoretical", "Empirical")
-    res
-  }  
+# setMethod("moments", signature(input = "numeric"), function(input) {
+#   if (length(input) == 1) {
+#     #input contains only number of positive partitions and total number of partitions
+#     moms(input)
+#   }
+#   if (length(input) == 2) {
+#     #input contains only number of positive partitions and total number of partitions
+#     moms(fl(input[1]/input[2]))
+#   } else {
+#     res <- cbind(moms(fl(sum(input > 0)/length(input))), empir_moms(input))
+#     colnames(res) <- c("Theoretical", "Empirical")
+#     res
+#   }  
+# })
+
+
+setMethod("moments", signature(input = "dpcr"), function(input) {
+  browser()
+  
+  summ <- summary(input, print = FALSE)[["summary"]]
+  kn <- summ[summ[["method"]] == "bhat", c("k", "n")]
+  
+  res <- rbind(do.call(cbind, lapply(1L:nrow(kn), function(single_row)
+    moms(kn[single_row, "k"], kn[single_row, "n"]))),
+    do.call(cbind, lapply(1L:nrow(kn), function(single_row)
+      empir_moms(kn[single_row, "k"], kn[single_row, "n"]))))
 })
 
 
-setMethod("moments", signature(input = "adpcr"), function(input) {
-  data <- slot(input, ".Data")
-  col_dat <-ncol(data)
-  type <- slot(input, "type")
-  n <- slot(input, "n")
-  
-  
-  switch(type,
-         tp = {
-           k <- data
-           vapply(k, function(x) moments(fl(x/n)), rep(0, 4))
-         },
-         nm = {
-           n_cols <- ncol(data)
-           res <- do.call(cbind, lapply(1L:n_cols, function(i) 
-             moments(data[1L:n[i], i])))
-           #need only two names, because they are repeating
-           nms <- colnames(res)[1:2]
-           rep_colnames <- unlist(lapply(colnames(data), function(i) rep(i, 2)))
-           colnames(res) <- unlist(lapply(1L:(n_cols * 2), function(i) 
-             paste0(nms[i %% 2 + 1], ".", rep_colnames[i])))
-           res
-         })  
-})
-
-setMethod("moments", signature(input = "ddpcr"), function(input) {
-  data <- slot(input, ".Data")
-  col_dat <- ncol(data)
-  type <- slot(input, "type")
-  n <- slot(input, "n")
-  
-  switch(type,
-         tp = {
-           k <- data
-           vapply(k, function(x) moments(fl(x/n)), rep(0, 8))
-         },
-         nm = {
-           n_cols <- ncol(data)
-           res <- do.call(cbind, lapply(1L:n_cols, function(i) 
-             moments(data[1L:n[i], i])))
-           #need only two names, because they are repeating
-           nms <- colnames(res)[1:2]
-           rep_colnames <- unlist(lapply(colnames(data), function(i) rep(i, 2)))
-           colnames(res) <- unlist(lapply(1L:(n_cols * 2), function(i) 
-             paste0(nms[i %% 2 + 1], ".", rep_colnames[i])))
-           res
-         },
-         fluo = {
-           k <- apply(data, 2, function(x) get_k_n(x, slot(input, "threshold")))
-           vapply(k, function(x) moments(fl(x/n)), rep(0, 4))
-         })  
-})
 
 
 #first four moments of distribution
-moms <- function(lambda) {
+#kn_df - data frame, row is run, first column is k, second is n
+moms <- function(k, n) {
+  lambda <- fl(k/n)
   res <- c(lambda, lambda, lambda^(-0.5), 1/lambda)
   names(res) <- c("mean", "var", "skewness", "kurtosis")
   res
 }
 
-empir_moms <- function(input) {
-  res <- c(mean(input), var(input), skewness(input), kurtosis(input))
+#sample moments
+empir_moms <- function(k, n) {
+  exp_kn <- c(rep(1, k), rep(0, n - k))
+  #res <- c(k/n, k(n - k)*n^-2, skewness(input), kurtosis(input))
+  res <- c(mean(exp_kn), var(exp_kn), skewness(exp_kn), kurtosis(exp_kn))
   names(res) <- c("mean", "var", "skewness", "kurtosis")
   res
 }
