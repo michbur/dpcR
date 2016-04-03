c4976a0c8b00f06a598682bca29aa185cdfeb817
michbur <michalburdukiewicz@gmail.com>
2016-04-03 10:22:50 +0200

    added df2dpcr

---
+++ R/df2dpcr.R
@@ -0,0 +1,57 @@
+#' Convert data.frame to dpcr object
+#' 
+#' Converts \code{\link{data.frame}} object to to \code{\linkS4class{adpcr}} or 
+#' \code{\linkS4class{ddpcr}} object. The resulting object will have \code{"tnp"}
+#' type.
+#' 
+#' @details The data frame must have following columns with exactly specified names:
+#' \describe{
+#' \item{experiment}{names of experiments}
+#' \item{replicate}{indices of replicates}
+#' \item{assay}{names of assays}
+#' \item{k}{number of positive partitions}
+#' \item{n}{total number of partitions}
+#' }
+#' There are also three optional columns:
+#' \describe{
+#' \item{row_names}{names of rows}
+#' \item{col_names}{names of columns}
+#' \item{panel_id}{indices of panels}
+#' }
+#' 
+#' If these additional columns are present, the resulting object has 
+#' \code{\linkS4class{adpcr}} type.
+#' @return An object of \code{\linkS4class{adpcr}} or \code{\linkS4class{ddpcr}} type, 
+#' depends on the presence of additional three columns with names of rows and columns of 
+#' panels as well as panel indices (see Details). 
+#' @seealso Flexibly create \code{\linkS4class{dpcr}} objects: \code{\link{create_dpcr}}
+#' @author Michal Burdukiewcz, Stefan Roediger
+#' @keywords utilities
+#' @examples
+#' dat <- data.frame(experiment = factor(rep(paste0("Experiment", 1L:2), 3)),
+#'                   replicate = c(1, 1, 2, 2, 3, 3),
+#'                   assay = "Assay1",
+#'                   k = c(55, 121, 43, 150, 70, 131),
+#'                  n = 765)
+#' df2dpcr(dat)
+#' @export
+
+df2dpcr <- function(df) {
+  if((!all(colnames(df) %in% c("experiment", "replicate", "assay", "k", "n")) && 
+      ncol(df) == 5) |
+     (!all(colnames(df) %in% c("experiment", "replicate", "assay", "k", "n",
+                               "row_names", "col_names", "panel_id")) && 
+      ncol(df) == 8))
+    stop("Wrong column names")
+  
+  create_dpcr(data = matrix(df[["k"]], nrow = 1),
+              n = df[["n"]],
+              exper = df[["experiment"]],
+              replicate = df[["replicate"]],
+              assay = df[["assay"]],
+              row_names = df[["row_names"]],
+              col_names = df[["col_names"]],
+              panel_id = df[["panel_id"]],
+              adpcr = ncol(df) == 8,
+              type = "tnp")
+}
