ac899f5d5c1db26892b3a9e06f0a9a727ab05ad2
michbur <michalburdukiewicz@gmail.com>
2016-12-01 15:46:56 +0100

    rename_dpcr now accepts not only factors

--- R/rename_dpcr.R
+++ R/rename_dpcr.R
@@ -3,12 +3,15 @@
 #' Renames objects of class \code{\linkS4class{adpcr}} or \code{\linkS4class{dpcr}}.
 #' 
 #' @param x an \code{\linkS4class{adpcr}} or \code{\linkS4class{dpcr}} object.
-#' @param exper a \code{factor} of new experiments' names. If \code{NULL},
+#' @param exper a vector of new experiments' names. If \code{NULL},
 #' experiments' names are not changed.
-#' @param replicate a \code{factor} of new replicates' ids. If \code{NULL},
+#' @param replicate a vector of new replicates' ids. If \code{NULL},
 #' replicates' names are not changed.
-#' @param assay a \code{factor} of new assays' names. If \code{NULL},
+#' @param assay a vector of new assays' names. If \code{NULL},
 #' assays' names are not changed.
+#' @details The valid \code{exper}, \code{replicate} and \code{assay} names 
+#' are factors. For the sake of convenience, this function converts other types 
+#' to factors if it is possible.
 #' @keywords manip
 #' @export
 
@@ -19,16 +22,28 @@ rename_dpcr <- function(x, exper = NULL, replicate = NULL, assay = NULL) {
     if(length(exper) == 1) {
       exper <- as.factor(rep(exper, ncol(slot(x, ".Data"))))
     }
+    if(!is.factor(exper)) {
+      exper <- as.factor(exper)
+      message("'exper' converted to factor.")
+    }
     slot(x, "exper") <- exper
   }
     
   if (!is.null(replicate)) {
+    if(!is.factor(replicate)) {
+      replicate <- as.factor(replicate)
+      message("'replicate' converted to factor.")
+    }
     slot(x, "replicate") <- replicate
   }
     
-  if (!is.null(assay))
+  if (!is.null(assay)) {
+    if(!is.factor(assay)) {
+      assay <- as.factor(assay)
+      message("'assay' converted to factor.")
+    }
     slot(x, "assay") <- assay
-  
+  }
   colnames(slot(x, ".Data")) <- paste0(slot(x, "exper"), ".", slot(x, "replicate"))
   x
 }
