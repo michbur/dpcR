5c4543c9214e38a361fabeef2f9d89038aa49559
michbur <michalburdukiewicz@gmail.com>
2016-12-11 17:10:19 +0100

    fixed missing zero values

--- R/calc_breaks.R
+++ R/calc_breaks.R
@@ -2,7 +2,7 @@
 calc_breaks <- function(vals, breaks = "Sturges", threshold = NULL) {
   if(length(unique(vals)) > 5) {
     br <- hist(vals, breaks = breaks, plot = FALSE)[["breaks"]]
-    cut(vals, breaks = br)
+    cut(vals, breaks = br, include.lowest = TRUE)
   } else {
     factor(vals)
   }
